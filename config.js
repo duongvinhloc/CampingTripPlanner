274fb3610d646e096119286443f69735a3c398fb
locdv <duongvinhloc@gmail.com>
2026-08-04 21:55:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ const IS_LOCAL = ['localhost', '127.0.0.1'].includes(location.hostname);
 // Dán URL Web App (kết thúc bằng /exec) sau khi deploy Apps Script vào đây.
 const APPS_SCRIPT_URL = IS_LOCAL
   ? ''
-  : 'https://script.google.com/macros/s/AKfycbxHoNqSBZFW2xKSfLq8DrIcpE-XnoLh9Wq-ng76hNddQR52tOmOeKmlaqEH6azdH8BW/exec';
+  : 'https://script.google.com/macros/s/AKfycbx1oekOflmkUg-mBj7kOilt-LNLRaw6EtDz9s1uODgRBvHWzr2il5sv9tYqJId2DemT/exec';
