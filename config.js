4aaea8601d98caac19bfdd568b6dcd6a0e45cc5b
locdv <duongvinhloc@gmail.com>
2026-08-04 21:48:59 +0900

    Update APPS_SCRIPT_URL to new deployment URL

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ const IS_LOCAL = ['localhost', '127.0.0.1'].includes(location.hostname);
 // Dán URL Web App (kết thúc bằng /exec) sau khi deploy Apps Script vào đây.
 const APPS_SCRIPT_URL = IS_LOCAL
   ? ''
-  : 'https://script.google.com/macros/s/AKfycbx.../exec';
+  : 'https://script.google.com/macros/s/AKfycbxHoNqSBZFW2xKSfLq8DrIcpE-XnoLh9Wq-ng76hNddQR52tOmOeKmlaqEH6azdH8BW/exec';
